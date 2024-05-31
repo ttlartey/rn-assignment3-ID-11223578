724331519a6a3df8c9215824d25ea46bddba738e
ttlartey <ttlartey@st.ug.edu.gh>
2024-05-31 17:13:48 +0000

    Adding React Native Project

---
+++ App.js
@@ -0,0 +1,32 @@
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import Frame1 from "./components/Frame1";
+import SearchBar from "./components/SearchBar";
+import CategorySection from "./components/CategorySection";
+import OngoingTask from "./components/OngoingTask";
+
+export default function App() {
+  return (
+    <>
+        <ScrollView>
+          <View style={styles.container}>
+            <Frame1/>
+            <SearchBar/>
+            <CategorySection/>
+            <OngoingTask/>
+          </View>
+        </ScrollView>
+
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start", 
+    alignItems: "flex-start",
+    paddingTop: 52, 
+    padding: 20, 
+    backgroundColor:"#F7F0E8"
+  }
+});
